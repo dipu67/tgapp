13f26a50a7065ada8f6156ea5700e38e3c103654
Dipu67 <mddipu2482@gmail.com>
2024-10-09 11:13:34 +0600

    modified:   app.js

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-const express = require('express')
-const socket = require('socket.io')
-const http = require('http')
-const app = express()
-const server = http.createServer(app)
-const io = socket(server)
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+require("dotenv").config();
+const BOT_TOKEN = process.env.TOKEN;
 
-app.set('view engine','ejs')
-app.use(express.json())
+app.set("view engine", "ejs");
+app.use(express.json());
 // app.use(express.urlencoded({extended:true}))
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.render('index') 
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.post(`/bot${BOT_TOKEN}`, async (req, res) => {
-    const { message } = req.body;
-  
-    if(message){
-      await handleMessage(message)
-    }
-    res.sendStatus(200);
-  });
+  const { message } = req.body;
 
-server.listen(4000,()=>{
-    console.log(`tgapp server is runging`);
-})
+  if (message) {
+    await handleMessage(message);
+  }
+  res.sendStatus(200);
+});
+
+server.listen(4000, () => {
+  console.log(`tgapp server is runging`);
+});
